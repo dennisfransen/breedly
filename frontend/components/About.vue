5f427949ee59af9db5117c7a5587b9140b2a26f0
Dennis Fransen <dennisfransen@Denniss-MacBook-Pro.local>
2019-06-03 14:29:56 +0200

    Home.vue About.vue And App.vue is ready for setup

---
+++ frontend/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>{{ msg }}</h1>
+        <p><router-link to="/">Home</router-link></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data() {
+        return {
+            msg: 'Welcome to About page!'
+        }
+    }
+}
+</script>
+
