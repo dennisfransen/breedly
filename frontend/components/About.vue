fb76e3fb06aa6796d1ff2f4227d30f9b11ec263e
Dennis Fransen <dennisfransen@Denniss-MacBook-Pro.local>
2019-06-04 13:55:50 +0200

    Added stuff to About.vue

--- frontend/components/About.vue
+++ frontend/components/About.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="about">
-        <h1>{{ msg }}</h1>
-        <p><router-link to="/">Home</router-link></p>
+
+        <v-container grid-list-md>
+            <v-layout row wrap>
+                <v-flex v-for="prop in aboutProps" :key="prop.text" xs4>
+                    <v-card>
+                        <v-img :src="prop.img" contain max-height="100"></v-img>
+                        <v-card-title class="display-2">{{ prop.text }}</v-card-title>
+                        <v-card-text class="pa-3">{{ msg }}</v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    
     </div>
 </template>
 
@@ -10,7 +21,11 @@ export default {
     name: 'home',
     data() {
         return {
-            msg: 'Welcome to About page!'
+            aboutProps: {
+                text: ["Vision", "Mission", "Values"],
+                img: ["./assets/eye-icon.png", "./assets/rocket-icon.png", "./assets/check-icon.png"]
+            },
+            msg: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus rem qui, sapiente autem asperiores labore praesentium ipsam non facilis porro suscipit eveniet libero illum iste cumque laudantium placeat vero unde.'
         }
     }
 }
--- frontend/index.js
+++ frontend/index.js
@@ -4,10 +4,7 @@ import Router from 'vue-router'
 import App from './components/App.vue'
 import Home from './components/Home.vue'
 import About from './components/About.vue'
-// import Navbar from './components/Navbar.vue'
 import 'vuetify/dist/vuetify.min.css'
-// import 'material-design-icons-iconfont/dist/material-design-icons.css' 
-// import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Router)
 Vue.use(Vuetify, {
@@ -17,7 +14,7 @@ Vue.use(Vuetify, {
 const router = new Router({
     routes: [
         { path: '/', name: 'home', component: Home },
-        { path: '/about', name: 'about', component: About }  
+        { path: '/about', name: 'about', component: About }
     ]
 })
 
