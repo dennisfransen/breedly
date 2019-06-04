c0bae1a4fd507ba828640ba4c941e0f20d7058f7
Dennis Fransen <dennisfransen@Denniss-MacBook-Pro.local>
2019-06-04 15:30:06 +0200

    Hello world

--- frontend/components/Home.vue
+++ frontend/components/Home.vue
@@ -1,48 +1,49 @@
 <template>
     <div class="home">
+    
         <v-layout row wrap>
-            <v-flex xs12 class="bg-img">
-                <v-img src="./assets/Dog_Background.jpg"></v-img>
-            </v-flex>
             <v-flex xs12 class="text-xs-center" mt-5>
                 <h1>Welcome to BREEDLY</h1>
             </v-flex>
             <v-flex xs12 sm6 offset-sm3 mt-3>
-                <blockquote class="blockquote text-xs-center"> 
+                <blockquote class="blockquote text-xs-center">
                     Our website is a revolutionary new dating platform specifically designed for animals,<br> 
                     where pet owners can find fluffy, prickly and cheerful partners with the help of our matematching search.
                 </blockquote>
             </v-flex>
             <v-flex xs12 sm6 offset-sm3 class="text-xs-center" mt-5>
-                <v-btn  big color="primary" to="/signup">GET STARTED</v-btn>
+                <v-btn big color="primary" to="/search">GET STARTED</v-btn>
             </v-flex>
         </v-layout>
-
+    
     </div>
 </template>
 
 <script>
-export default {
-    name: 'home',
-    data() {
-        return {
-            msg: 'Welcome to home page!'
+    export default {
+        name: 'home',
+        data() {
+            return {}
         }
     }
-}
 </script>
 
 <style scoped>
-
-    .photoSettings{
-        position: relative;
-        background-size: 100%;
+    .home {
+        background-image: url('../assets/Dog_Background.jpg');
+        background-size: cover;
+        height: 100%;
     }
-    .textSettings{
-        position: absolute; 
-        top: 200px; 
-        left: 0; 
-        width: 100%; 
-    }
-
+    
+    
+    /* .photoSettings{
+            position: relative;
+            background-size: 100%;
+        }
+        .textSettings{
+            position: absolute; 
+            top: 200px; 
+            left: 0; 
+            width: 100%; 
+        } */
 </style>
--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,30 +1,30 @@
- <template>
+<template>
   <nav>
     <v-toolbar app>
-      
+  
       <router-link class="mr-3 underline" to="/">
-      <v-toolbar-title class="text-uppercase display-1 grey--text">
-        <span class="font-weight-bold">Breedly</span>
-      </v-toolbar-title>
+        <v-toolbar-title class="text-uppercase display-1 grey--text">
+          <span class="font-weight-bold">Breedly</span>
+        </v-toolbar-title>
       </router-link>
-      
-      <router-link class="mr-3" v-for="link in links" :key="link.name" :to="link.path" >
+  
+      <router-link class="mr-3" v-for="link in links" :key="link.name" :to="link.path">
         <v-toolbar-title class="grey--text">
           <v-btn flat class="font-weight-light navItem">{{link.name}}</v-btn>
         </v-toolbar-title>
       </router-link>
-
+  
       <v-spacer></v-spacer>
-
+  
       <v-btn flat @click="login = !login">Login</v-btn>
-      
-     
+  
+  
       <!-- <v-flex xs12 md4 lg2>
-        <v-expansion-panel>
-        <v-expansion-panel-content>
-          <template v-slot:header>
-            <div>Login</div>
-          </template>
+          <v-expansion-panel>
+          <v-expansion-panel-content>
+            <template v-slot:header>
+              <div>Login</div>
+</template>
           <v-card>
             <v-card-text>
               <input placeholder="email">
@@ -38,9 +38,10 @@
      
   <!-- <v-expansion-panel>
     <v-expansion-panel-content v-for="link in links" :key="link">
-      <template v-slot:header>
-        <div>Item</div>
-      </template>
+<template v-slot:header>
+  <div>
+    Item</div>
+</template>
       <v-card>
         <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
       </v-card>
@@ -68,12 +69,20 @@
   export default {
     data() {
       return {
-        links: [
-          { path: '/about', name: 'About us'},
-          { path: '/info', name: 'How does Breedly work?'},
-          { path: '/contact', name: 'Contact us'},
-          ],
-          login: false
+        links: [{
+            path: '/about',
+            name: 'About us'
+          },
+          {
+            path: '/info',
+            name: 'How does Breedly work?'
+          },
+          {
+            path: '/contact',
+            name: 'Contact us'
+          },
+        ],
+        login: false
       }
     }
   }
@@ -84,13 +93,16 @@
     text-decoration: none;
     text-transform: uppercase;
   }
+  
   a:hover,
-  a:focus{
+  a:focus {
     color: #eee;
   }
+  
   .underline {
     text-decoration: underline;
   }
+  
   .marginLogin {
     margin-top: 100px;
   }
