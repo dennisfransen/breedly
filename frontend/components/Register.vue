1a3eee8a14b6f4b381cb544634934414ebe440bb
Cawehp <cawehpassereh@hotmail.se>
2019-06-07 13:38:05 +0200

    frontend additions to register page

--- frontend/components/Register.vue
+++ frontend/components/Register.vue
@@ -6,7 +6,7 @@
           <v-layout justify-center>
             <v-flex v-for="n in 1" :key="n" xs4 text-xs-center>
               <!-- card for user registration info -->
-              <v-card flat tile>
+              <v-card v-if="!nextInfo" flat tile>
                 
                 <v-text-field v-model="email" label="E-mail" required></v-text-field>
                 <v-text-field v-model="password" label="Password" :type="'password'" required></v-text-field>
@@ -28,6 +28,26 @@
                 <v-btn @click="next(email, password, name, number)" flat> Next </v-btn>
               
               </v-card>
+             
+              <!-- Pet information form -->
+              <v-card v-if="nextInfo" flat tile>
+
+                <v-text-field v-model="petType" label="Pet type" required></v-text-field>
+                <v-text-field v-model="petName" label="Pet name" required></v-text-field>
+                <v-text-field v-model="petAge" label="Pet age" required></v-text-field>
+                
+                <v-textarea v-model="petDescription" label="Describe your pet">Description</v-textarea>
+
+                <v-switch v-model="pedigree" label="Pedigree"></v-switch>
+                
+                <v-switch @click="petGender(gender)" v-model="petMale" label="Male"></v-switch> 
+                <v-switch @click="petGender(!gender)" v-model="petFemale" label="Female"></v-switch>
+                
+                <v-btn @click="back()" flat> Back </v-btn>
+                <v-btn @click="registerPet(petType, petName, petAge, petDescription, pedigree)" flat> Register </v-btn>
+
+              </v-card>
+            
             </v-flex>
           </v-layout>
         </v-container>
@@ -70,7 +90,17 @@
         email: null,
         number: null,
         location: 'County',
-        userInfo: null
+        userInfo: null,
+        nextInfo: false,
+        petName: null,
+        petType: null,
+        petAge: null,
+        petDescription: null,
+        pedigree: null,
+        gender: null,
+        petMale: null,
+        petFemale: null,
+        petInfo: null,
       }
     },
     methods: {
@@ -78,6 +108,7 @@
         console.log(this.countyArray[index])
         this.location = this.countyArray[index]
       },
+      // Get user information and save to user object
       next(email, password, name, number) {
         this.userInfo = {
           name: name,
@@ -86,13 +117,35 @@
           number: number,
           location: this.location
         }
-        console.log(this.userInfo)
+        this.nextInfo = true
         this.saveUser(this.userInfo)
       },
+      back() {
+        this.nextInfo = false
+      },
+      registerPet(type, name, age, description, pedigree) {
+        this.petInfo = {
+          type: type,
+          name: name,
+          age: age,
+          description: description,
+          pedigree: pedigree
+        }
+        console.log(this.petInfo)
+      },
+      petGender(gender) {
+        if(gender) {
+          this.petMale = false
+          this.petFemale = true
+        } else if(!gender) {
+          this.petMale = true
+          this.petFemale = false
+        }
+      },
+      // Save user to database
       saveUser(userInfo) {
-        fetch('http://localhost:8080/register', {
-                body: '{ ' + userInfo + '}',
-                // body: '{ "name": "' + this.name + '", "password": ' + this.password + ', "email": ' + this.email + ', "location": ' + this.location + ', "number": ' + this.number + '}',
+        fetch('/register', {
+                body: '{ "name": "' + this.name + '", "password": ' + this.password + ', "email": ' + this.email + ', "location": ' + this.location + ', "number": ' + this.number + '}',
                 headers: {
                 'Content-Type': 'application/json'
                 },
@@ -101,7 +154,7 @@
             .then(result => {
                 console.log('saved user', result)
             })
-      }
+      },
     }
   }
 </script>
