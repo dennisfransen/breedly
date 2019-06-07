db923c2f98b16c52a5a8f4034bf92434a106a7dc
ASH <ashee-90@hotmail.com>
2019-06-07 13:27:07 +0200

    background and card done

--- frontend/components/Register.vue
+++ frontend/components/Register.vue
@@ -1,47 +1,42 @@
 <template>
   <div class="register">
   
-
-        
-    <v-layout mt-5>
+    <v-layout>
       <v-flex xs12 sm6 offset-sm3>
-
-          <v-card color="rgba(0, 0, 0, 0.2">
-
-
-            <v-container v-bind="{ [`grid-list-${0}`]: true }" fluid>
-              <v-layout justify-center>
-                <v-flex v-for="n in 1" :key="n" xs4 text-xs-center>
-
-                  <!-- card for user registration info -->
-                  <v-card color="rgba(0, 0, 0, 0.0" flat tile>
-                        
-                        <v-text-field v-model="email" label="E-mail" required></v-text-field>
-                        <v-text-field v-model="password" label="Password" :type="'password'" required></v-text-field>
-                        <v-text-field v-model="name" label="Full name" required></v-text-field>
-                        <v-text-field v-model="number" label="Phone number" required></v-text-field>
-                        
-                        <!-- scroll list for location -->
-                        <v-menu offset-x> 
-                        <template v-slot:activator="{ on }">
-                          <v-btn v-on="on" flat> {{ location }}</v-btn>
-                        </template>
+  
+        <v-card class="mt-5" color="rgba(0, 0, 0, 0.2">
+  
+          <v-container v-bind="{ [`grid-list-${0}`]: true }" fluid>
+            <v-layout justify-center>
+              <v-flex v-for="n in 1" :key="n" xs4 text-xs-center>
+  
+                <!-- card for user registration info -->
+                <v-card color="rgba(0, 0, 0, 0.0" flat tile>
+  
+                  <v-text-field dark v-model="email" label="E-mail" required></v-text-field>
+                  <v-text-field dark v-model="password" label="Password" :type="'password'" required></v-text-field>
+                  <v-text-field dark v-model="name" label="Full name" required></v-text-field>
+                  <v-text-field dark v-model="number" label="Phone number" required></v-text-field>
+  
+                  <!-- scroll list for location -->
+                  <v-menu offset-x>
+                    <template v-slot:activator="{ on }">
+                      <v-btn  v-on="on" > {{ location }} <v-icon color="red" right="" >location_on</v-icon> </v-btn>
+                    </template>
                   <v-list style="max-height: 300px" class="scroll-y">
                     <v-list-tile v-for="(county, index) in countyArray" :key="index" @click="chooseCounty(index)">
                       <v-list-tile-title>{{ county }}</v-list-tile-title>
                     </v-list-tile>
-                    </v-list>
-                </v-menu>
+                  </v-list>
+                  </v-menu>
 
-                <v-btn @click="register(email, password, name)">Register<v-icon color="green" right="" >check_circle</v-icon></v-btn>
+                  <v-btn  @click="register(email, password, name)">Next<v-icon color="green" right="" >arrow_forward</v-icon></v-btn>
               
-              </v-card>
+                </v-card>
             </v-flex>
           </v-layout>
         </v-container>
       </v-card>
-
-
     </v-flex>
   </v-layout>
 
