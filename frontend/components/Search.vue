4b0711e689c2b8249bb47eef93b7a1cbddcd4d74
Dennis Fransen <dennisfransen@denniss-mbp.home>
2019-06-08 15:23:44 +0200

    Small changes

--- frontend/components/Search.vue
+++ frontend/components/Search.vue
@@ -89,3 +89,12 @@
         }
     }
 </script>
+
+<style scoped>
+     .search {
+         background-image: url("../assets/grass-two.png");
+         background-repeat: no-repeat;
+         background-size: auto 40em;
+         background-position: bottom; 
+     } 
+</style>
