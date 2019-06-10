22115e90b15c573cb8a8bb17c59e8cfc78b6f5f1
Dennis Fransen <dennisfransen@hotmail.se>
2019-06-10 14:30:32 +0200

    Filter function on searcbar almost done

--- frontend/components/Search.vue
+++ frontend/components/Search.vue
@@ -4,21 +4,14 @@
       <v-layout row wrap>
         <v-flex xs12>
           <v-card dark color="success" class="px-2 pt-4">
-            <v-text-field
-              append-icon="mic"
-              class="mx-3"
-              flat
-              label="Search"
-              prepend-inner-icon="search"
-              solo-inverted
-            ></v-text-field>
+            <v-text-field append-icon="mic" class="mx-3" flat label="Search" prepend-inner-icon="search" solo-inverted v-model="searchText"></v-text-field>
           </v-card>
         </v-flex>
-
+  
         <v-layout row wrap>
           <v-flex mt-3 xs9>
             <div>
-              <v-expansion-panel v-model="panel" expand>
+              <v-expansion-panel expand>
                 <v-expansion-panel-content v-for="animal in pets" :key="animal.name">
                   <template v-slot:actions>
                     <v-icon small>fas fa-chevron-up</v-icon>
@@ -30,6 +23,7 @@
                       <p>{{ animal.type }}</p>
                     </div>
                   </template>
+
                   <v-card>
                     <v-card-text class="grey lighten-3">{{ animal.description }}</v-card-text>
                   </v-card>
@@ -44,12 +38,12 @@
               <v-divider class="mx-3"></v-divider>
 
               <v-container fluid>
-                <v-switch color="success" v-model="switch1" :label="`Female`"></v-switch>
-                <v-switch color="success" v-model="switch2" :label="`Male`"></v-switch>
+                <v-switch color="success" @click="filter" v-model="switchFemale" :label="`Female`"></v-switch>
+                <v-switch color="success" v-model="switchMale" :label="`Male`"></v-switch>
 
                 <v-divider></v-divider>
 
-                <v-switch color="success" v-model="switch3" :label="`Pedigree`"></v-switch>
+                <v-switch color="success" v-model="switchPedigree" :label="`Pedigree`"></v-switch>
 
                 <v-btn @click="clearFilter">Clear filter</v-btn>
               </v-container>
@@ -62,41 +56,63 @@
 </template>
 
 <script>
-export default {
-  created() {
-    this.fetchPets();
-  },
-  data() {
-    return {
-      switch1: true,
-      switch2: true,
-      switch3: true,
-      pets: null
-    };
-  },
-  methods: {
-    clearFilter() {
-      this.switch1 = false;
-      this.switch2 = false;
-      this.switch3 = false;
+  export default {
+    created() {
+      this.fetchPets();
     },
-    fetchPets() {
-      fetch("http://localhost:3000/pets")
-        .then(response => response.json())
-        .then(result => {
-          this.pets = result;
-          console.log("fetchPets works!");
-        });
-    }
-  }
-};
+    data() {
+      return {
+        switchFemale: true,
+        switchMale: true,
+        switchPedigree: true,
+        constPets: null,
+        pets: null,
+        searchText: null,
+        tempPet: []
+      };
+    },
+    methods: {
+      clearFilter() {
+        this.switchFemale = false;
+        this.switchMale = false;
+        this.switchPedigree = false;
+      },
+      fetchPets() {
+        fetch("http://localhost:3000/pets")
+          .then(response => response.json())
+          .then(result => {
+            this.pets = result;
+            this.constPets = result;
+            console.log("fetchPets works!");
+          });
+      }
+    },
+     watch: {
+      searchText: function () {
+        let text = this.searchText
+        let dict = this.tempPet
+        dict = []
+
+        this.pets.filter( (pet) => {
+          if (text.length < 1) {
+            this.pets = this.constPets
+          } else if (pet.name.includes(text)) {
+            dict.push(pet)
+            this.pets = dict
+          }
+
+        })
+      }
+    },
+  };
 </script>
 
 <style scoped>
-     .search {
-         background-image: url("../assets/grass-two.png");
-         background-repeat: no-repeat;
-         background-size: auto 40em;
-         background-position: bottom; 
-     } 
+  .search {
+    /* Multer */
+    /* background-image: url("../assets/grass-two.png"); */
+    background-repeat: no-repeat;
+    background-size: auto 40em;
+    background-position: bottom;
+  }
 </style>
