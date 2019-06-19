82be6c9fd82b24d0971a72d48f0450841b629ed3
bejan <beyhanbresner@gmail.com>
2019-06-19 18:10:00 +0200

    display contact email and phone number from DB

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const sqlite = require('sqlite')
 const bodyParser = require('body-parser')
-const uuidv4 = require('uuid/v4')
-const multer = require('multer')
-const path = require('path')
-var cookieParser = require('cookie-parser')
+const uuidv4 = require('uuid/v4');
 
 const app = express()
 let database
-var upload = multer({dest: 'images/'})
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,178 +13,62 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use('/images', express.static(path.join(path.resolve(), 'images')))
 app.use(bodyParser.json())
-app.use(cookieParser())
 
 sqlite.open('db.sqlite').then(database_ => {
   database = database_
 })
 
-app.post('/petImage')
-
 console.log('Database connection up and running')
 // Query for search.vue, show all animals in DB
 app.get('/pets', (request, response) => {
-  
   database.all('SELECT * FROM pet').then(pets => {
     response.send(pets)
+    console.log('All animals in db are: ');
+    console.log(pets);
+
   })
 })
-
 // Query for login.vue, check if email and password in DB
 app.get('/users/:userEmail/:userPassword', (request, response) => {
-  
-  database.all('SELECT * FROM user').then(users => { 
-    var tempUser = null
-    var userFound = false
-    var cookieId = null
+  database.all('SELECT * FROM user').then(users => {
+    console.log(users);
 
-    for (let i = 0; i < users.length; i++) {
-      tempUser = users[i];
-
-      if (request.params.userEmail === tempUser.email && request.params.userPassword === tempUser.password) {
-        cookieId = uuidv4()
-        userFound = true
-        response.status(200)
-        request.params.userId
-        break
-      } else if (request.params.userEmail === tempUser.email && request.params.userPassword !== tempUser.password) {
+    users.forEach(user => {
+      if (request.params.userEmail === user.email && request.params.userPassword === user.password) {
+        console.log('user and password exsists in DB');
+        response.send()
+      } else if (request.params.userEmail === user.email && request.params.userPassword !== user.password) {
+        console.log('email found in DB, wrong password');
         response.status(401)
-        break
+        response.send()
       } else {
+        console.log(request.params.userEmail, request.params.userPassword)
+        console.log('user and password doesnt exsist in DB')
         response.status(404)
+        response.send()
       }
-    }
-    
-    if (!userFound) {
-      response.send('')
-    } else {
-      database.run('INSERT INTO cookieMonster VALUES (?,?)', [tempUser.id, cookieId]).then(() => {
-        response.cookie('id', cookieId).send(tempUser.name)
-        
-        
-      })
-    }
+    })
   })
-})
 
-app.delete('/signout', (request, response) => {
-
-  database.run('DELETE FROM cookieMonster WHERE secretId=?', [request.cookies.id])
-  response.clearCookie(request.cookies.id);
-  response.send()
-})
-
-// Query for app.vue to get cookie if user is already logged in
-app.get('/getCookies', (request, response) => {
-  database.all('SELECT user.name FROM user INNER JOIN cookieMonster ON user.id = cookieMonster.userId').then(user =>{
-    if (user[0].name === undefined) {
-      response.send('')
-    } else {
-      response.send(user[0].name)
-    }
-  })
-})
-
-// Query for app.vue to get user id if user is already logged in
-app.get('/getUserId', (request, response) => {
-  database.all('SELECT user.id FROM user INNER JOIN cookieMonster ON user.id = cookieMonster.userId').then(user =>{
-    if (user[0].id === undefined) {
-      response.send('')
-    } else {
-      response.send(user[0].id)
-    }
-  })
-})
+});
 
 app.get('/contacts', (request, response) => {
-  const contacts = [
-    { email: 'randy@random.com', phone: '1834214567' },
-    { email: 'hardy@random.com', phone: '1958023452' },
-  ]
-  response.send(contacts)
-})
-
-app.get('/users', (request, response) => {
-  database.all('SELECT * FROM user')
-  .then(users => {
-    response.send(users)
-    
-  })
- })
- 
-app.post('/file', upload.single('image'), (request, response) => {
-  response.send(request.file)
+  database.all('SELECT * FROM contactus').then(contacts => {
+    response.send(contacts)
+  });
 })
 
 app.post('/users', (request, response) => {
-  var tempId = uuidv4()
-  database.run('INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)', [request.body.name, request.body.password, request.body.email, request.body.location, request.body.number, tempId])
-  .then(() => {
-    var cookieId = uuidv4()
-    database.run('INSERT INTO cookieMonster VALUES (?,?)', [tempId, cookieId]).then(() => {
-      response.cookie('id', cookieId).send(tempId)
-    })
-  })
+  console.log(request.body)
+  database.run('INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)', [request.body.name, request.body.password, request.body.email, request.body.location, request.body.number, uuidv4()])
 })
 
 app.post('/pets', (request, response) => {
-  database.run('INSERT INTO pet VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [request.body.name, request.body.userId, request.body.type, request.body.description, request.body.gender, request.body.pedigree, request.body.age, request.body.imageName])
-  .then(() => {
-    response.send('inserted!')
-  })
-})
-
-
-
-
-
-
-
-
-
-
-app.get('/userCredentials', (request, response) => {
-
-  database.all('SELECT user.* FROM user INNER JOIN cookieMonster ON user.id = cookieMonster.userId')
-  .then(userCredentials => { 
-    
-    response.send(userCredentials)
-
-  })
-
-})
-
-app.get('/userPets', (request, response) => {
-
-  database.all('SELECT pet.* FROM pet INNER JOIN cookieMonster ON pet.userid = cookieMonster.userId')
-  .then(petInfo => {
-
-    console.log('this is petinfo: ', petInfo)
-    response.send(petInfo)
-
-  })
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/aboutText', (request, response) => {
-  database.all('SELECT * FROM aboutus')
-  .then(aboutText => {
-    response.send(aboutText)
-  })
+  database.run('INSERT INTO pet VALUES (?, ?, ?, ?, ?, ?, ?)', [request.body.name, uuidv4(), request.body.type, request.body.description, request.body.gender, request.body.pedigree, request.body.age])
 })
 
 app.listen(3000)
+
+
+
--- frontend/components/Contact.vue
+++ frontend/components/Contact.vue
@@ -1,15 +1,12 @@
 <template>
-  <v-layout row>
-    <v-flex xs12 sm6 offset-sm3>
+  <!-- <v-layout class ="contactus" row>
+    <v-flex xs11 sm6 offset-sm3>
       <v-card>
-        <v-toolbar color="#696969" dark>
-          <v-toolbar-title>Contact Us</v-toolbar-title>
+        <v-toolbar class ="translucentBackground" color="#696969" dark>
+          <v-toolbar-title >Contact Us</v-toolbar-title>
 
           <v-spacer></v-spacer>
 
-          <v-btn icon>
-            <v-icon>search</v-icon>
-          </v-btn>
         </v-toolbar>
 
         <v-list two-line>
@@ -31,11 +28,9 @@
               v-else
               :key="contact.title"
               avatar
-            >
-              <v-list-tile-avatar>
+            ><v-list-tile-avatar>
                 <img :src="contact.avatar">
               </v-list-tile-avatar>
-
               <v-list-tile-content>
                 <v-list-tile-title v-html="contact.email"></v-list-tile-title>
                 <v-list-tile-sub-title v-html="contact.phone"></v-list-tile-sub-title>
@@ -45,24 +40,84 @@
         </v-list>
       </v-card>
     </v-flex>
-  </v-layout>
+  </v-layout>-->
+
+
+  <div class="login" align-center justify-center fill-height>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card class="mt-5" color="rgba(0, 0, 0, 0.2">
+          <v-layout justify-right offset-sm6>
+            <v-flex xs6 sm3 text-xs-right offset-sm8>
+              <template v-for="(contact, index) in contacts">
+                <v-subheader
+                  v-if="contact.email"
+                  :key="contact.email"
+                >{{ contact.email }} - {{contact.number}}</v-subheader>
+              </template>
+            </v-flex>
+          </v-layout>
+
+          <v-layout justify-right offset-sm1>
+            <v-flex xs6 sm3 text-xs-right offset-sm1>
+              <!-- card for userinput -->
+              <v-card class="translucentBackground" flat tile>
+                <v-text-field dark v-model="email" label="Email" required></v-text-field>
+                <v-textarea
+                  dark
+                  outline
+                  v-model="title"
+                  label="Message us"
+                  counter
+                  maxlength="150"
+                  full-width
+                  single-line
+                ></v-textarea>
+
+                <v-btn dark outline>
+                  Send
+                  <v-icon color="green" right>mail</v-icon>
+                </v-btn>
+              </v-card>
+              <!-- card for error message when email dosen't exsist -->
+              <v-card v-if="notRegistered" flat tile class="translucentBackground">
+                <v-divider></v-divider>
+                <div class="white--text">We can't find an account with this email!</div>
+              </v-card>
+              <!-- card for error message when password is wrong -->
+              <v-card v-if="wrongPassword" flat tile class="translucentBackground">
+                <v-divider></v-divider>
+                <div class="white--text">That password ain't right, dog.</div>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
+
+
+
+
 <script>
 export default {
-    data() {
-        return {
-            contacts: []
-        }
-    },
-    methods: {
-        async fetchContacts() {
-            this.contacts = await fetch("http://localhost:3000/contacts").then(response => response.json())
-        }
-    },
-    created() {
-        this.fetchContacts()
+  data() {
+    return {
+      contacts: []
+    };
+  },
+  methods: {
+    async fetchContacts() {
+      this.contacts = await fetch("http://localhost:3000/contacts").then(
+        response => response.json()
+      );
     }
-}
+  },
+  created() {
+    this.fetchContacts();
+  }
+};
 </script>
 
 <style scoped>
@@ -76,7 +131,11 @@ export default {
 
 .translucentBackground {
   margin-top: 5px;
-  background-color: rgba(0, 0, 0, 0.0);
+  background-color: rgba(0, 0, 0, 0);
 }
+/* .formSettings{
+    background-color: gray;
+    position: relative;
+} */
 </style>
 
